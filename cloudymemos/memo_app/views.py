b4bf699eba127240e1832eb24b806b24851a93f1
Branislav Pekar <pekar@rt-rk.com>
2025-10-21 13:41:17 +0200

    Registration - Create new user form added

--- cloudymemos/memo_app/views.py
+++ cloudymemos/memo_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm
 
-# Create your views here.
 
 def homepage(request):
     
@@ -9,7 +9,23 @@ def homepage(request):
 
 def register(request):
     
-    return render(request, 'memo_app/register.html')
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+            
+            return redirect('my-login')
+
+        
+        
+    context = {'RegistrationForm': form}
+    
+    return render(request, 'memo_app/register.html', context)
 
 
 def my_login(request):
